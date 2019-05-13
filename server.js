e3952b55d3b2f67f71c50795c00147dd780438c9
José Carlos Clausen Neto <deora.2@hotmail.com>
2019-05-13 10:56:48 -0300

    Added support to user queries

--- server.js
+++ server.js
@@ -1,40 +1,65 @@
+'use strict';
+
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
-const user = {};
-user.name = 'Yooooo';
-user.age = '25';
 
+let id = 0;
 
+const providers = {
+   users: []
+};
+
+// Queries de consultas; utilizam os métodos
+//    dos providers.
 var schema = buildSchema(`
-  type Query {
-    hello: String
-    me: User
-  }
+   type User {
+      id: ID
+      name: String
+      repo: String
+      age: String
+   }
 
-  type User {
-     id: ID
-     name: String
-     age: String
-  }
+   type Query {
+      hello: String
+      user(id: ID!): User
+      users: [User]
+   }
+
+   type Mutation {
+      createUser(name: String!, repo: String!, age: Int!): User
+   }
 `);
 
-function Query_me(request) {
-   return user;
-}
+const resolvers = {
+   user({id}) {
+      return providers.users
+         .find(el => el.id === Number(id));
+   },
+   users() {
+      return providers.users;
+   },
+   createUser({name, repo, age}) {
+      const user = {
+         id: id++,
+         name,
+         repo,
+         age
+      };
 
-function User_name(user) {
-   return user.name;
-}
+      providers.users.push(user);
 
-var root = { hello: () => 'Hello world!' };
+      return user;
+   }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
-  graphiql: true,
+  rootValue: resolvers,
+  graphiql: false,
 }));
+
 app.listen(
    3000,
     () => console.log('Now browse to localhost:3000/graphql'));
